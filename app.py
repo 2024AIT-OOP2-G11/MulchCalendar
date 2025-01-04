407b7d892e5d84f05b713a9c4b322e7b94450d51
papurika8 <314.miyako.314@gmail.com>
2025-01-05 00:31:43 +0900

    カレンダー表示1

--- app.py
+++ app.py
@@ -1,3 +1,19 @@
-if __name__ == '__main__':
-    pass
-  
+from flask import Flask, render_template
+import calendar
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    # 現在の年月を取得
+    year = datetime.now().year
+    month = datetime.now().month
+    # 月ごとのカレンダーを取得
+    cal = calendar.monthcalendar(year, month)
+    # テンプレートに渡して表示
+    return render_template("index.html", year=year, month=month, calendar=cal)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
