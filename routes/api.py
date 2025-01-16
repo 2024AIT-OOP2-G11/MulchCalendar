b9c0eace9a7c2910447a887d9f4b6f2f406dd734
yu-shink23037 <yasashin620@gmail.com>
2025-01-16 14:05:43 +0900

    Merge branch 'main' of https://github.com/2024AIT-OOP2-G11/MultiCalendar

--- routes/api.py
+++ routes/api.py
@@ -1,5 +1,6 @@
 from flask import Blueprint,jsonify
 from models import Event
+from models import User
 
 api_bp = Blueprint("api", __name__, url_prefix='/api')
 
@@ -15,3 +16,15 @@ def get_events():
         'borderColor': event.user.color,
         
     } for event in events])
+    
+@api_bp.route('/user/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    # ここでデータベースからユーザー情報を取得
+    user = User.get_or_none(User.id == user_id)
+    print("ok")
+    if user:
+        return jsonify({
+            'user': user.user,
+            'color': user.color
+        })
+    return
