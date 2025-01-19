0064808457f1f442b7a74f20429a8e6f2f26fb39
yu-jimk <142887865+yu-jimk@users.noreply.github.com>
2025-01-19 11:34:24 +0900

    update:初期状態を全選択に変更

--- routes/api.py
+++ routes/api.py
@@ -6,9 +6,6 @@ api_bp = Blueprint("api", __name__, url_prefix='/api')
 
 @api_bp.route('/events', methods=['GET'])
 def get_events():
-    # DBにデータを追加
-    # Event.create(title='イベント1',start='2025-01-10',end='2025-01-10',user=1)
-    # Event.create(title='イベント2',start='2025-01-11',end='2025-01-11',user=2)
 
     events = Event.select()
 
